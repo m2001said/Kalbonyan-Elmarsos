4fd8e21a8013cf0a8de53a95944800efc0dc0ba5
Mohamed Said <91760639+m2001said@users.noreply.github.com>
2022-08-03 06:20:02 +0200

    Add files via upload

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/redux-cart/src/components/Cart/CartButton.js
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuality = useSelector((state) => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuality}</span>
+    </button>
+  );
+};
+
+export default CartButton;
