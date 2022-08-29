9239afba5dd9a27eabfe54ac4fed6a149f66d6e5
Mohamed Said <91760639+m2001said@users.noreply.github.com>
2022-08-29 19:01:44 +0200

    Add files via upload

---
+++ 02-Udemy/-02-Js-Jonas/Challenges/JavaScript Fundamentals – Part 2/04-Challenges/script.js
@@ -0,0 +1,25 @@
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  // adding values to tips array
+  tips.push(tip);
+  // adding values to total array
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
