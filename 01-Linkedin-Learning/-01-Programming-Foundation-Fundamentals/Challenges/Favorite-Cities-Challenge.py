ce0026637d80a30a1dc047828bc92dd1b33a22a1
Mohamed Said <91760639+m2001said@users.noreply.github.com>
2022-08-29 12:46:46 +0200

    Add files via upload

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
@@ -1,1 +1,11 @@
 
+#make the function (which called creating the function)
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+#calling the function because if you don't call it , it will not appear
+#It's required to call it 3 times
+favorite_city("Santa Barbara, California")
+favorite_city("Asheville, North Carolina")
+favorite_city("Amsterdam, The Netherlands")
+
